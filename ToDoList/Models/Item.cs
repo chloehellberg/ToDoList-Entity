ef5d4f7c264ce934a00d3bac505aae9ad2450ed1
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-06 10:56:43 -0700

    Add Entity framework and reconfigured files to work with it. Added HTML helper methods and updated Controllers.
    Co-authored by: Natalie Murphy <nataliemurphy500@gmail.com>
    Co-authored-by: Grant Eadie <grantleadie@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,26 +1,11 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
   public class Item
   {
+    public int ItemId { get; set; }
     public string Description { get; set; }
-    private static List<Item> _instances = new List<Item> { };
-
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-    }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
   }
 }

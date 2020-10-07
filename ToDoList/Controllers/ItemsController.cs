6e2dc1e44de5f6fd3907d0ea523e16136699527c
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-07 10:22:47 -0700

    Add one to many relationship functionality.
    Co-authored by: Natalie Murphy <nataliemurphy500@gmail.com>
    Co-authored-by: Grant Eadie <grantleadie@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -15,11 +17,12 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
     [HttpPost]
@@ -34,6 +37,35 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items.FirstOrDefault(ItemsController => ItemsController.ItemId == id);
       return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
